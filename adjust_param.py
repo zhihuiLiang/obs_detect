e4741d33951e7b0cfe2c9184153cba8135e8f0a4
zhihuiLiang <1518705978@qq.com>
2023-12-21 15:04:01 +0800

    add perspective info

--- adjust_param.py
+++ adjust_param.py
@@ -1,24 +1,44 @@
 import cv2
 import numpy as np
+from detect import drawConvex
+from detect import findConvexPoints
 
-cap = cv2.VideoCapture(0)
+USE_VIDEO = True
+
+if not USE_VIDEO:
+    cap = cv2.VideoCapture(0)
+else:
+    cap = cv2.VideoCapture('origin_video.mp4')
 if not cap.isOpened():
-    print("Cannot open camera")
+    print("Cannot open camera or video")
     exit()
 
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 print(f'size:{width}×{height}')
 
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter('origin_vedio.mp4',
-                      fourcc=fourcc,
-                      fps=30,
-                      frameSize=(width, height))
 
-
-def do_nothing(x):
+def doNothing(x):
     pass
+
+
+brick_width, brick_height = 480, 480
+four_corner_points = []
+rotate_mat = np.float32()
+
+
+def onMouseClick(event, x, y, flags, param):
+    global rotate_mat
+    if event == cv2.EVENT_LBUTTONDOWN:  # 左键点击
+        four_corner_points.append([x, y])
+        print(f"Mouse clicked at (x={x}, y={y})")
+    if len(four_corner_points) == 4:
+        vertices = np.float32([[0, 0], [brick_width, 0],
+                               [brick_width, brick_height], [0, brick_height]])
+
+        rotate_mat = cv2.getPerspectiveTransform(
+            np.float32(four_corner_points), vertices)
+        print('rotate mat:', rotate_mat)
 
 
 window_name = 'hsv_threshold'
@@ -27,19 +47,27 @@ cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
 #h: [0, 10] and [156, 180]
 #s: [43, 255]
 #v: [46, 255]
-cv2.createTrackbar('h1_max', window_name, 0, 180, do_nothing)
-cv2.createTrackbar('h1_min', window_name, 0, 180, do_nothing)
-cv2.createTrackbar('h2_max', window_name, 0, 180, do_nothing)
-cv2.createTrackbar('h2_min', window_name, 0, 180, do_nothing)
-cv2.createTrackbar('s_max', window_name, 0, 255, do_nothing)
-cv2.createTrackbar('s_min', window_name, 0, 255, do_nothing)
-cv2.createTrackbar('v_max', window_name, 0, 255, do_nothing)
-cv2.createTrackbar('v_min', window_name, 0, 255, do_nothing)
+cv2.createTrackbar('h1_min', window_name, 0, 180, doNothing)
+cv2.createTrackbar('h1_max', window_name, 20, 180, doNothing)
+cv2.createTrackbar('h2_min', window_name, 140, 180, doNothing)
+cv2.createTrackbar('h2_max', window_name, 180, 180, doNothing)
+cv2.createTrackbar('s_max', window_name, 255, 255, doNothing)
+cv2.createTrackbar('s_min', window_name, 51, 255, doNothing)
+cv2.createTrackbar('v_max', window_name, 255, 255, doNothing)
+cv2.createTrackbar('v_min', window_name, 140, 255, doNothing)
+
+_, frame = cap.read()
 
 while True:
-    _, frame = cap.read()
-    cv2.imshow("A video", frame)
+    cv2.imshow("frame", frame)
+    cv2.setMouseCallback("frame", onMouseClick)
 
+    if len(four_corner_points) == 4:
+        perspective_img = cv2.warpPerspective(frame, rotate_mat,
+                                              (brick_width, brick_height))
+        cv2.imshow('perspective', perspective_img)
+
+    contour_img = np.copy(frame)
     hsv_img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     h1_max = cv2.getTrackbarPos('h1_max', window_name)
@@ -58,13 +86,18 @@ while True:
 
     binary_img = mask0 + mask1
 
-    cv2.imshow('Binary', binary_img)
+    contours, hierachy = cv2.findContours(binary_img, cv2.RETR_CCOMP,
+                                          cv2.CHAIN_APPROX_SIMPLE)
+    for i, contour in enumerate(contours):
+        hull = cv2.convexHull(contour)
+        points = findConvexPoints(hull)
+        drawConvex(points, contour_img)
 
-    ret = out.write(frame)
-    c = cv2.waitKey(1)
+    cv2.imshow('Binary', binary_img)
+    cv2.imshow('contour', contour_img)
+
+    c = cv2.waitKey(10)
     if c == 27:
         break
 
-out.release()
-cap.release()
 cv2.destroyAllWindows()
--- detect.py
+++ detect.py
@@ -1,14 +1,23 @@
+from pickletools import uint8
 import cv2
 import numpy as np
 import json
 
 ENABLE_BLUR = False
+USE_VIDEO = True
 
-H1_MAX = 10
-H2_MIN = 143
-S_MIN = 51
-S_MAX = 201
-V_MIN = 186
+# H1_MAX = 20
+# H2_MIN = 143
+# S_MIN = 51
+# S_MAX = 201
+# V_MIN = 186
+# V_MAX = 255
+
+H1_MAX = 30
+H2_MIN = 140
+S_MIN = 70
+S_MAX = 255
+V_MIN = 140
 V_MAX = 255
 
 BLUR_SIZE = 5
@@ -18,9 +27,14 @@ MIN_BOX_CONTOUR = 200
 MIN_NUN_CONTOUR = 20
 MAX_NUM_CONTOUR = 2000
 
-cap = cv2.VideoCapture(0)
+MAP_ROI_SIZE = (480, 480)
+
+if not USE_VIDEO:
+    cap = cv2.VideoCapture(0)
+else:
+    cap = cv2.VideoCapture('origin_video.mp4')
 if not cap.isOpened():
-    print("Cannot open camera")
+    print("Cannot open camera or video")
     exit()
 
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -42,15 +56,10 @@ shape = conf_data['shape']
 
 cam_mat = np.transpose(conf_data['cam_mat'])
 distor_coffe = np.array(conf_data['distortion'])
+rotate_mat = np.float32(conf_data['rotate_mat'])
 
-def findConvexPoints(hull):
-    x_min_index = np.argmin(hull[:, :, 0])
-    x_max_index = np.argmax(hull[:, :, 0])
-    y_min_index = np.argmin(hull[:, :, 1])
-    y_max_index = np.argmax(hull[:, :, 1])
-    index = [x_min_index, y_min_index, x_max_index, y_max_index]
-    points = [hull[i][0] for i in index]
 
+def sortedPoints(points):
     length = []
     for i, p in enumerate(points):
         p2 = points[(i + 1) % 4]
@@ -63,18 +72,28 @@ def findConvexPoints(hull):
     for _ in range(4):
         sorted_points.append(points[shortest_index % 4])
         shortest_index += 1
-
     return sorted_points
 
 
-def drawConvex(points, img):
+def findConvexPoints(hull):
+    x_min_index = np.argmin(hull[:, :, 0])
+    x_max_index = np.argmax(hull[:, :, 0])
+    y_min_index = np.argmin(hull[:, :, 1])
+    y_max_index = np.argmax(hull[:, :, 1])
+    index = [x_min_index, y_min_index, x_max_index, y_max_index]
+    points = [hull[i][0] for i in index]
+
+    return sortedPoints(points)
+
+
+def drawConvex(points, img, color):
     assert (len(points) == 4)
     for i, p in enumerate(points):
-        cv2.line(img, (p[0], p[1]),
-                 (points[(i + 1) % 4][0], points[(i + 1) % 4][1]), (0, 255, 0),
-                 1)
-        cv2.putText(img, str(i), (p[0], p[1]), cv2.FONT_HERSHEY_SIMPLEX, 1,
-                    (255, 0, 0), 1)
+        p1 = (int(p[0]), int(p[1]))
+        p2 = (int(points[(i + 1) % 4][0]), int(points[(i + 1) % 4][1]))
+        cv2.line(img, p1, p2, color, 1)
+        # cv2.putText(img, str(i), p1, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0),
+        #             1)
 
 
 def extractNumImg(points, img):
@@ -113,8 +132,8 @@ def classifyNum(img):
     id1, conf1 = classify(img)
     id2 = 0
     conf2 = 0.0
-    if id == 8 or conf1 < 0.7:
-        img = cv2.flip(img, 0)
+    if id1 == 8 or conf1 < 0.7:
+        img = cv2.flip(img, 1)
         id2, conf2 = classify(img)
     return id1 if conf1 > conf2 or id2 == 8 else id2
 
@@ -129,6 +148,7 @@ def doPnP(idx, points_in_img):
                         np.float32(cam_mat), np.float32(distor_coffe), None,
                         None, False, cv2.SOLVEPNP_EPNP)
 
+
 def rotateVec2EulerAnge(rvec):
     rmat = cv2.Rodrigues(rvec)
     ''' ZYX式旋转 先yaw、后pitch、在roll
@@ -140,68 +160,86 @@ def rotateVec2EulerAnge(rvec):
        sin(pitch) * sin(yaw) * cos(roll) + sin(roll) * cos(yaw),
       cos(pitch) * cos(roll), 0]
       [0, 0, 0, 1]]
-    ''' 
+    '''
 
-while True:
-    _, frame = cap.read()
-    height, width, _ = frame.shape
-    result_img = np.copy(frame)
 
-    gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    hsv_img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+if __name__ == '__main__':
+    while True:
+        _, frame = cap.read()
+        height, width, _ = frame.shape
+        result_img = np.copy(frame)
 
-    mask0 = cv2.inRange(hsv_img, np.array([0, S_MIN, V_MIN]),
-                        np.array([H1_MAX, S_MAX, V_MAX]))
-    mask1 = cv2.inRange(hsv_img, np.array([H2_MIN, S_MIN, V_MIN]),
-                        np.array([255, S_MAX, V_MAX]))
+        gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        hsv_img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    binary_img = mask0 + mask1
-    cv2.imshow('Binary', binary_img)
-    blur_img = binary_img
-    if ENABLE_BLUR:
-        blur_img = cv2.blur(binary_img, (BLUR_SIZE, BLUR_SIZE))
-        cv2.imshow('Blur', blur_img)
+        mask0 = cv2.inRange(hsv_img, np.array([0, S_MIN, V_MIN]),
+                            np.array([H1_MAX, S_MAX, V_MAX]))
+        mask1 = cv2.inRange(hsv_img, np.array([H2_MIN, S_MIN, V_MIN]),
+                            np.array([255, S_MAX, V_MAX]))
 
-    contour_img = np.copy(frame)
-    contours, hierachy = cv2.findContours(blur_img, cv2.RETR_CCOMP,
-                                          cv2.CHAIN_APPROX_SIMPLE)
-    
-    cv2.drawContours(contour_img, contours, -1, (0, 255, 0), 1)
-    cv2.imshow('contour', contour_img)
-    for i, contour in enumerate(contours):
-        contour_area = cv2.contourArea(contour)
-        if contour_area < MIN_NUN_CONTOUR or contour_area > MAX_NUM_CONTOUR or hierachy[0][i][3] == -1: 
-            # print(f'box contourArea:{cv2.contourArea(contour)} too small or this contour do not have parenet contour')
-            continue
-        num_hull = cv2.convexHull(contour)
-        num_points = findConvexPoints(num_hull)
-        drawConvex(num_points, result_img)
-        num_image = extractNumImg(num_points, frame)
-        id = classifyNum(num_image)
-        cv2.putText(result_img, str(id),
-                    (num_points[0][0] + 10, num_points[0][1]),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 1)
-        if id == 8:
-            continue
-        
-        box_contour_idx = hierachy[0][i][3]
-        box_contour = contours[box_contour_idx]
-        print(f'num contourArea:{cv2.contourArea(box_contour)}')
-        box_hull = cv2.convexHull(box_contour)
-        box_points = findConvexPoints(box_hull)
-        drawConvex(box_points, result_img)
- 
-        # ok, r_vec, t_vec = doPnP(id, num_points)
-        # rotateVec2EulerAnge(r_vec)
+        binary_img = mask0 + mask1
+        cv2.imshow('Binary', binary_img)
+        blur_img = binary_img
+        if ENABLE_BLUR:
+            blur_img = cv2.blur(binary_img, (BLUR_SIZE, BLUR_SIZE))
+            cv2.imshow('Blur', blur_img)
 
-    cv2.imshow("origin", frame)
-    cv2.imshow('result', result_img)
+        obs_loc_arr = []
+        binary_map = np.ones((height, width, 3), dtype=np.uint8) * 255
+        contour_img = np.copy(frame)
+        contours, hierachy = cv2.findContours(blur_img, cv2.RETR_CCOMP,
+                                              cv2.CHAIN_APPROX_SIMPLE)
 
-    ret = out.write(frame)
-    c = cv2.waitKey(1)
-    if c == 27:
-        break
+        cv2.drawContours(contour_img, contours, -1, (0, 255, 0), 1)
+        cv2.imshow('contour', contour_img)
+        for i, contour in enumerate(contours):
+            contour_area = cv2.contourArea(contour)
+            if contour_area < MIN_NUN_CONTOUR or contour_area > MAX_NUM_CONTOUR or hierachy[
+                    0][i][3] == -1:
+                # print(f'box contourArea:{cv2.contourArea(contour)} too small or this contour do not have parenet contour')
+                continue
+            num_rect = cv2.minAreaRect(contour)
+            num_hull = cv2.boxPoints(num_rect)
+            num_points = sortedPoints(num_hull)
+            drawConvex(num_points, result_img, (0, 255, 0))
+            num_image = extractNumImg(num_points, frame)
+            id = classifyNum(num_image)
+            cv2.putText(result_img, str(id),
+                        (int(num_points[0][0] + 10), int(num_points[0][1])),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1)
 
-out.release()
-cap.release()
-cv2.destroyAllWindows()
+            box_contour_idx = hierachy[0][i][3]
+            box_contour = contours[box_contour_idx]
+            box_rect = cv2.minAreaRect(box_contour)
+            box_hull = cv2.boxPoints(box_rect)
+            box_points = sortedPoints(box_hull)
+            drawConvex(box_points, result_img, (0, 255, 0))
+
+            obs_loc_arr.append(box_points)
+            # ok, r_vec, t_vec = doPnP(id, num_points)
+            # rotateVec2EulerAnge(r_vec)
+
+        cv2.imshow("origin", frame)
+        cv2.imshow('result', result_img)
+        perspective_obs_loc = []
+        for obs_loc in obs_loc_arr:
+            # drawConvex(obs_loc, binary_map, (0, 0, 0))
+            homo_point = np.hstack((obs_loc, np.ones((len(obs_loc), 1))))
+            perspective_point = rotate_mat @ np.transpose(homo_point)
+            perspective_obs_loc.append(np.transpose(perspective_point[:2, :]))
+
+        binary_map = cv2.warpPerspective(binary_map, rotate_mat, MAP_ROI_SIZE)
+        for points in perspective_obs_loc:
+            drawConvex(points, binary_map, (0, 0, 0))
+        cv2.imshow('map', binary_map)
+
+        if not USE_VIDEO:
+            ret = out.write(frame)
+
+        c = cv2.waitKey(1)
+        if c == 27:
+            break
+
+    out.release()
+    cap.release()
+    cv2.destroyAllWindows()
